54830bd5b0ee38e0bfe0bcdf257c7257f0dc9a0e
Jayone Lee <kenbugless@gmail.com>
2024-12-31 15:22:09 +0900

    feat:di설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:image_search_app/data/data_source/pixabay_api.dart';
-import 'package:image_search_app/data/repository/photo_api_repository_impl.dart';
-import 'package:image_search_app/domain/use_case/get_photos_use_case.dart';
+import 'package:image_search_app/di/provider_setup.dart';
 import 'package:image_search_app/presentation/home/home_screen.dart';
-import 'package:image_search_app/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: globalProviders,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,11 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => HomeViewModel(GetPhotosUseCase(
-            PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
-        child: HomeScreen(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
